4dada805bf02dbd3ca90bf206fccc5c20b587e51
Weleson Batista <weleson.dev@gmail.com>
2024-11-07 10:10:18 -0300

    feat: first commit

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Meu Portfolio</title>
+</head>
+<body>
+    <h1>
+        <?php
+
+            $saudacao = "Hello";
+        
+            echo $saudacao . ", Weleson Batista";
+
+        ?>
+    </h1>
+    <p>
+        <?php
+        echo "weleson weleson";
+        ?>
+    </p>
+</body>
+</html>
